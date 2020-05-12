387dd19b41296dcb606224aed4c70ad66c37e346
Shreevathsa Gorur <shreevathsagorur@Shreevathsa-GP-Macbook.local>
2020-05-12 23:23:44 +0530

    Initial Commit

--- thing.py
+++ thing.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
-
-# Just in case
-@app.route('/<random>')
-def random(random):
-    return redirect(url_for('home'))
 
 @app.route('/')
 def home():
     return render_template('pathfinding.html')
 
+@app.route('/cron')
+def cron():
+    return "OK!"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
